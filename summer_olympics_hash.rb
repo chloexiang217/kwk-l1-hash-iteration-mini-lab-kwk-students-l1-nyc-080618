041df6ad4fbef0e61e6d9ced6f14dd7a0ede207f
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-08 19:38:28 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -17,9 +17,11 @@ def iterate_through_hash
 end
 
 def iterate_through_keys
-  
+  upcase_cities = add_a_key_value_pair.map 
 end
 
 
 # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  
+  # Use the each_key method to return an array of the cities where each city name has been upcased. Save this to a new variable called upcased_cities and puts this to the screen.
