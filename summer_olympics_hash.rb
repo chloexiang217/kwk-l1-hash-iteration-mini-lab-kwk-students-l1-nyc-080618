d2a29a35173d5b17ceb3ae43665b3eb3eae5244f
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-08 19:41:41 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,12 +13,12 @@ new_hash
 end
 
 def iterate_through_hash
-  add_a_key_value_pair.each {|key, value| puts "The #{value} summer olympics took place in #{key}"
+  add_a_key_value_pair.each do {|key, value| puts "The #{value} summer olympics took place in #{key}"
 end
 
 def iterate_through_keys
   upcase_cities = add_a_key_value_pair.map {|key, value| key.upcase}
-  upcase_cities.each {city|puts city}
+  upcase_cities.each do {city|puts city}
 end
 
 
