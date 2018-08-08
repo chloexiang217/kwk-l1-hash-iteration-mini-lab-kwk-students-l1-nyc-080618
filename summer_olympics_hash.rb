950a25a2be3cea9aa0aa069214e0790a87894886
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-08 19:39:48 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -17,7 +17,8 @@ def iterate_through_hash
 end
 
 def iterate_through_keys
-  upcase_cities = add_a_key_value_pair.map 
+  upcase_cities = add_a_key_value_pair.map {|key, value| key.upcase}
+  upcase_cities.each {city|puts city}
 end
 
 
